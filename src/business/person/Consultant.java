7c82ee4cd8ac543ff83dba3dcfeb88f48b8abaf4
Murat Karyağdı <murat@uncosoft.com>
2018-10-30 15:50:17 +0300

    employee and consultant toString

--- src/business/person/Consultant.java
+++ src/business/person/Consultant.java
@@ -6,4 +6,8 @@ public class Consultant extends Person {
         super(id, name);
     }
 
+    @Override
+    public String toString() {
+        return "Id: " + this.getId() + ", name: " + this.getName() + " (" + this.getClass().getName() + ")";
+    }
 }
