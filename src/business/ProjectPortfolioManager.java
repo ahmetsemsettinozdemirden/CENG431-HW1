4a965437d681ba91d3b988d3b5bf7d8d183f9023
Murat Karyağdı <murat@uncosoft.com>
2018-10-30 15:20:39 +0300

    assign and unassign resource exceptions

--- src/business/ProjectPortfolioManager.java
+++ src/business/ProjectPortfolioManager.java
@@ -100,7 +100,7 @@ public class ProjectPortfolioManager {
 
     public void assign(Resource resource, Task task) {
         if (task.getResource().equals(resource)) {
-            return; // TODO: throw exception (resouce already assigned)
+            throw new ResourceAlreadyAssignedException("Task already assigned to the this resource");
         } else {
             task.setResource(resource);
             resource.getTasks().add(task);
@@ -112,7 +112,7 @@ public class ProjectPortfolioManager {
             task.setResource(null);
             resource.getTasks().remove(task);
         } else {
-            return; // TODO: throw exception (no assigned resource)
+            throw new ResourceNotFoundException("Task does not have a resource to unassign");
         }
     }
 
@@ -147,6 +147,12 @@ public class ProjectPortfolioManager {
 
     public static class ResourceNotFoundException extends RuntimeException {
         public ResourceNotFoundException(String error) {
+            super(error);
+        }
+    }
+
+    public static class ResourceAlreadyAssignedException extends RuntimeException {
+        public ResourceAlreadyAssignedException(String error) {
             super(error);
         }
     }
